221b5343dcc40b7e5bfe546d0e92284c059263c4
Jisun You <jisunyou@Jisuns-MacBook-Pro.local>
2017-12-18 23:10:07 -0600

    typo

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -165,7 +165,7 @@ var work = {
       "title": "LEGAL ADMINISTRATOR",
       "location": "Seoul, South Korea",
       "dates": "October 2004 - August 2009",
-      "summery": "Worked in Telecommunications, Media and Technology group. Enhanced attorneys' effectiveness by providing administrative support.",
+      "summery": "Worked for Telecommunications, Media and Technology group. Enhanced attorneys' effectiveness by providing administrative support.",
       "description": [
 	      "Communicated with clients and other staffs so that attorneys can work according to plan.",
 	      "Assisted lawyers with preparation of pleadings, affidavits, briefs and court applications. Collected, organized and sorted litigation files and other legal documentation. Maintained and updated files.",
